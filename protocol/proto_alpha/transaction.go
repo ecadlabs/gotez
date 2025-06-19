7ac94240606c3669da4ab0c7b9747474141afc6a
Eugene Zagidullin <e.asphyx@gmail.com>
2025-06-19 22:37:04 +0300

    ListPseudoOperations

--- protocol/proto_alpha/transaction.go
+++ protocol/proto_alpha/transaction.go
@@ -4,3 +4,5 @@ import "github.com/ecadlabs/gotez/v2/protocol/proto_022_PsRiotum"
 
 type Transaction = proto_022_PsRiotum.Transaction
 type TransactionContentsAndResult = proto_022_PsRiotum.TransactionContentsAndResult
+
+var ListPseudoOperations = proto_022_PsRiotum.ListPseudoOperations
